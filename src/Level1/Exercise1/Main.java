93c92460afd473cd9550e276a2238ca84f585912
Enric <reiben815@gmail.com>
2024-10-31 12:23:00 +0100

    Add Execise1 code

--- src/Level1/Exercise1/Main.java
+++ src/Level1/Exercise1/Main.java
@@ -1,7 +1,53 @@
 package Level1.Exercise1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ArrayList<Month> year = new ArrayList<Month>();
+
+        System.out.println("Adding months to the year...");
+        year.add(new Month("January"));
+        year.add(new Month("February"));
+        year.add(new Month("March"));
+        year.add(new Month("April"));
+        year.add(new Month("May"));
+        year.add(new Month("June"));
+        year.add(new Month("July"));
+        year.add(new Month("September"));
+        year.add(new Month("October"));
+        year.add(new Month("November"));
+        year.add(new Month("December"));
+
+        System.out.println("The year has " + year.size() + " months.");
+        System.out.println("The months are:");
+        for (Month month : year) {
+            System.out.println(month.getName()+" as month number "+(year.indexOf(month)+1));
+        }
+
+        System.out.println("Adding August...");
+        year.add(7, new Month("August"));
+
+        System.out.println("The year has " + year.size() + " months.");
+        System.out.println("The months are:");
+        for (Month month : year) {
+            System.out.println(month.getName()+" as month number "+(year.indexOf(month)+1));
+        }
+
+        HashSet<Month> newYear = new HashSet<>(year);
+
+        System.out.println("The new year has " + newYear.size() + " months.");
+        System.out.println("The months are:");
+        Iterator<Month> it = newYear.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next().getName()+" as month number "+(year.indexOf(it.next())+1));
+        }
+        System.out.println("The new year has " + newYear.size() + " months.");
+        System.out.println("The months are:");
+        for (Month month : newYear) {
+            System.out.println(month.getName()+" as month number "+(year.indexOf(month)+1));
+        }
     }
 }
---
+++ src/Level1/Exercise1/Month.java
@@ -0,0 +1,13 @@
+package Level1.Exercise1;
+
+public class Month {
+    public String name;
+
+    public Month(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
