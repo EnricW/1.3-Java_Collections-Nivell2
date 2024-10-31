8738c17c8ace807a7305864c6a1ff440c51f1282
Enric <reiben815@gmail.com>
2024-10-31 12:39:56 +0100

    Add Execise2 code

--- src/Level1/Exercise2/Main.java
+++ src/Level1/Exercise2/Main.java
@@ -1,7 +1,30 @@
 package Level1.Exercise2;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Creating a list of numbers from 1 to 10");
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 1; i <= 10; i++) {
+            numbers.add(i);
+        }
+
+        ListIterator<Integer> iterator = numbers.listIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("Creating a countdown from 10 to 1, using the list of numbers");
+        ArrayList<Integer> countdown = new ArrayList<Integer>();
+        while (iterator.hasPrevious()) {
+            countdown.add(iterator.previous());
+        }
+
+        System.out.println("The countdown:");
+        for (int number : countdown) {
+            System.out.println(number);
+        }
     }
 }
