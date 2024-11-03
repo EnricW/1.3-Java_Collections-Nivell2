7ca81ca3fc1e4da350435680092f8d7315b21606
reiben815 <reiben815@gmail.com>
2024-11-03 13:49:30 +0100

    Improve Level1.Exercise2 code

--- src/Level1/Exercise2/Main.java
+++ src/Level1/Exercise2/Main.java
@@ -9,15 +9,13 @@ public class Main {
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         for (int i = 1; i <= 10; i++) {
             numbers.add(i);
-        }
-
-        ListIterator<Integer> iterator = numbers.listIterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+            System.out.println(numbers.get(numbers.size() - 1));
         }
 
         System.out.println("Creating a countdown from 10 to 1, using the list of numbers");
         ArrayList<Integer> countdown = new ArrayList<Integer>();
+
+        ListIterator<Integer> iterator = numbers.listIterator(numbers.size());
         while (iterator.hasPrevious()) {
             countdown.add(iterator.previous());
         }
